e2967dbc6557a98a07a4a10999b7eadc2d5183f5
dylanshangold <dylan.shangold@gmail.com>
2024-12-02 17:02:24 -0500

    dynamic image

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,21 @@
 import "./WeatherCard.css";
-import sunny from "../../assets/sunny.png";
+import { weatherOptions } from "../../utils/constants";
+// import sunny from "../../assets/sunny.png";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  const weatherOptionUrl = filteredOptions[0]?.url;
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg;</p>
-      <img src={sunny} alt="sunny" className="weather-card__image" />
+      <img src={weatherOptionUrl} alt="sunny" className="weather-card__image" />
     </section>
   );
 }
