364d9a8a92395b323f90703bad80f3a97c1d9d3f
dylanshangold <dylan.shangold@gmail.com>
2024-12-02 20:56:05 -0500

    modal style

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -4,9 +4,7 @@ function ItemModal({ activeModal, onClose, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
-        <button onClick={onClose} className="modal__close">
-          CLOSE
-        </button>
+        <button onClick={onClose} className="modal__close"></button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
