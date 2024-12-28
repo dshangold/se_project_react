4ba6a1b326b9c5fb6e206005ccbc7d982119bd41
dylanshangold <dylan.shangold@gmail.com>
2024-12-28 16:44:49 -0500

    update temp context

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,8 +1,10 @@
 import "./WeatherCard.css";
 import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
-// import sunny from "../../assets/sunny.png";
+import { useContext } from "react";
+import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   const filteredOptions = weatherOptions.filter((option) => {
     return (
       option.day === weatherData.isDay &&
@@ -20,7 +22,10 @@ function WeatherCard({ weatherData }) {
 
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg;</p>
+      <p className="weather-card__temp">
+        {weatherData.temp[currentTemperatureUnit]} &deg;
+        {currentTemperatureUnit}
+      </p>
       <img
         src={weatherOption?.url}
         alt={`Card Showing ${weatherOption?.day ? "day" : "night"}time ${
